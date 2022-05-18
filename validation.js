0f7a1f95397a623918a795de2fcec33c7cceaf3d
MAmiruddin <m.amiruddinazizan@gmail.com>
2022-05-18 09:25:21 +0800

    Modified registration validation

--- validation.js
+++ validation.js
@@ -4,7 +4,7 @@ const Joi = require('@hapi/joi');
 //Registration Validation
 const registerValidation = data => {
      const schema = Joi.object({
-          name: Joi.string().min(6).required(),
+          name: Joi.string().min(6).required().alphanum(),
           email: Joi.string().min(6).required().email(),
           password: Joi.string().min(6).required(),
           tenant_id: Joi.objectId().required()
